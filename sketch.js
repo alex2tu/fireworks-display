02a94e4a1eded613c1204d90c3c287f90276a2c1
alex2tu <alexandertu0215@gmail.com>
2023-09-23 00:56:13 -0400

    add makefirework and killparticles
    wrote functions to make fireworks at click and delete particles that are out of range

--- sketch.js
+++ sketch.js
@@ -1,5 +1,3 @@
-
-
 // let particle;
 let particles;
 
@@ -9,26 +7,28 @@ function setup() {
   
   particles = []
   
-  for (let i = 0; i < 250; i++) {
-    let p = new Particle();
-    p.x = width/2;
-    p.y = height/2;
+  makeFirework(width/2,height/2);
+  
+//   for (let i = 0; i < 250; i++) {
+//     let p = new Particle();
+//     p.x = width/2;
+//     p.y = height/2;
     
-    // p.velX = random(-1,1); //cartesian velocities
-    // p.velY = random(-1,1);
+//     // p.velX = random(-1,1); //cartesian velocities
+//     // p.velY = random(-1,1);
     
-    let angle = random(0,2*PI);
-    let vel = random(0,1);
+//     let angle = random(0,2*PI);
+//     let vel = random(0,1);
     
-    p.velX = vel * cos(angle);
-    p.velY = vel * sin(angle);
+//     p.velX = vel * cos(angle);
+//     p.velY = vel * sin(angle);
     
-    p.r = random(0,255)
-    p.g = random(0,255)
-    p.b = random(0,255)
+//     p.r = random(0,255)
+//     p.g = random(0,255)
+//     p.b = random(0,255)
     
-    particles.push(p)
-  }
+//     particles.push(p)
+  // }
   
   // particle = new Particle();
   //particle.x = 0
@@ -51,9 +51,52 @@ function draw() {
     particles[i].velY += 0.0098;
   }
   
+  killParticles();
+  
   // circle(variable,height/2, 100);
   // circle(width/3,height/1.5,50);
   
   // variable++;
   
+}
+
+function makeFirework(x,y) {
+  // particles = []
+  
+  for (let i = 0; i < 50; i++) {
+    let p = new Particle();
+    p.x = x;
+    p.y = y;
+    
+    // p.velX = random(-1,1); //cartesian velocities
+    // p.velY = random(-1,1);
+    
+    let angle = random(0,2*PI);
+    let vel = random(0,1);
+    
+    p.velX = vel * cos(angle);
+    p.velY = vel * sin(angle);
+    
+    p.r = random(0,255)
+    p.g = random(0,255)
+    p.b = random(0,255)
+    
+    particles.push(p)
+  }
+  
+
+}
+
+function mousePressed() {
+  makeFirework(mouseX,mouseY)
+}
+
+function killParticles() {
+  let aliveParticles = []
+  for (let i = 0; i < particles.length; i++) {
+    if (particles[i].y <= height) {
+      aliveParticles.push(particles[i])
+    }
+  }
+  particles = aliveParticles;
 }
