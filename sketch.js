6ded1daca1e1e0c89dcae07c69b2f67180b37d70
alex2tu <alexandertu0215@gmail.com>
2023-09-23 01:03:45 -0400

    updated vals
    changed random vels and gravity

--- sketch.js
+++ sketch.js
@@ -2,37 +2,12 @@
 let particles;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1800, 1200);
   noStroke(); //no outlines
   
   particles = []
   
   makeFirework(width/2,height/2);
-  
-//   for (let i = 0; i < 250; i++) {
-//     let p = new Particle();
-//     p.x = width/2;
-//     p.y = height/2;
-    
-//     // p.velX = random(-1,1); //cartesian velocities
-//     // p.velY = random(-1,1);
-    
-//     let angle = random(0,2*PI);
-//     let vel = random(0,1);
-    
-//     p.velX = vel * cos(angle);
-//     p.velY = vel * sin(angle);
-    
-//     p.r = random(0,255)
-//     p.g = random(0,255)
-//     p.b = random(0,255)
-    
-//     particles.push(p)
-  // }
-  
-  // particle = new Particle();
-  //particle.x = 0
-  //partifle.y = 0
 }
 
 function draw() {
@@ -44,20 +19,14 @@ function draw() {
   for (let i = 0; i < particles.length; i++) {
     fill(particles[i].r,particles[i].g,particles[i].b, transparency)
 
-    circle(particles[i].x,particles[i].y,10)
+    circle(particles[i].x,particles[i].y,8)
     particles[i].x += particles[i].velX;
     particles[i].y += particles[i].velY;
     
-    particles[i].velY += 0.0098;
+    particles[i].velY += 0.01;
   }
   
   killParticles();
-  
-  // circle(variable,height/2, 100);
-  // circle(width/3,height/1.5,50);
-  
-  // variable++;
-  
 }
 
 function makeFirework(x,y) {
@@ -72,7 +41,7 @@ function makeFirework(x,y) {
     // p.velY = random(-1,1);
     
     let angle = random(0,2*PI);
-    let vel = random(0,1);
+    let vel = random(0.2,1.5);
     
     p.velX = vel * cos(angle);
     p.velY = vel * sin(angle);
