886af5db732c55304c1ae85bb0407a6c4f93e690
alex2tu <alexandertu0215@gmail.com>
2023-09-23 02:06:28 -0400

    fixed indents

--- sketch.js
+++ sketch.js
@@ -17,13 +17,13 @@ function draw() {
     let transparency = 150
 
     for (let i = 0; i < particles.length; i++) {
-    fill(particles[i].r,particles[i].g,particles[i].b, transparency)
+        fill(particles[i].r,particles[i].g,particles[i].b, transparency)
 
-    circle(particles[i].x,particles[i].y,8)
-    particles[i].x += particles[i].velX;
-    particles[i].y += particles[i].velY;
+        circle(particles[i].x,particles[i].y,8)
+        particles[i].x += particles[i].velX;
+        particles[i].y += particles[i].velY;
 
-    particles[i].velY += 0.01;
+        particles[i].velY += 0.01;
     }
 
     killParticles();
@@ -54,9 +54,9 @@ function mousePressed() {
 function killParticles() {
     let aliveParticles = []
     for (let i = 0; i < particles.length; i++) {
-    if (particles[i].y <= height) {
-        aliveParticles.push(particles[i])
-    }
+        if (particles[i].y <= height) {
+            aliveParticles.push(particles[i])
+        }
     }
     particles = aliveParticles;
 }
@@ -68,22 +68,22 @@ function randomShape() {
 
 function standardFirework(x,y) {
     for (let i = 0; i < 50; i++) {
-    let p = new Particle(x,y);
+        let p = new Particle(x,y);
 
-    // p.velX = random(-1,1); //cartesian velocities
-    // p.velY = random(-1,1);
+        // p.velX = random(-1,1); //cartesian velocities
+        // p.velY = random(-1,1);
 
-    let angle = random(0,2*PI);
-    let vel = random(0.2,1.5);
+        let angle = random(0,2*PI);
+        let vel = random(0.2,1.5);
 
-    p.velX = vel * cos(angle);
-    p.velY = vel * sin(angle);
+        p.velX = vel * cos(angle);
+        p.velY = vel * sin(angle);
 
-    p.r = random(0,255)
-    p.g = random(0,255)
-    p.b = random(0,255)
+        p.r = random(0,255)
+        p.g = random(0,255)
+        p.b = random(0,255)
 
-    particles.push(p)
+        particles.push(p)
     }
 }
 
@@ -93,29 +93,29 @@ function starFirework(x,y) {
     b = random(0,255)
     let starAngleOffset = random(PI/2, -PI/2);
     for (let i = 0; i < 75; i++) {
-    let p = new Particle(x,y);
+        let p = new Particle(x,y);
 
-    let m = 3
-    let n = 5
-    let k = 1
-    let angle = random(0,2*PI);
-    // let t = angle / 180 * PI;
-    let t = angle + starAngleOffset;
-    let numerator = cos((2 * asin(k) + PI*m) / (2*n)) 
-    let denominator = cos((2*asin(k*cos(n*t))+PI*m)/(2*n))
-    let radius = numerator / denominator;
+        let m = 3
+        let n = 5
+        let k = 1
+        let angle = random(0,2*PI);
+        // let t = angle / 180 * PI;
+        let t = angle + starAngleOffset;
+        let numerator = cos((2 * asin(k) + PI*m) / (2*n)) 
+        let denominator = cos((2*asin(k*cos(n*t))+PI*m)/(2*n))
+        let radius = numerator / denominator;
 
-    // let radius = sin(angle)
-    let vel = radius
+        // let radius = sin(angle)
+        let vel = radius
 
-    p.velX = cos(angle) * vel;
-    p.velY = sin(angle) * vel;
+        p.velX = cos(angle) * vel;
+        p.velY = sin(angle) * vel;
 
-    p.r = r
-    p.g = g
-    p.b = b
+        p.r = r
+        p.g = g
+        p.b = b
 
-    particles.push(p)
+        particles.push(p)
     }
 }
   
@@ -124,22 +124,22 @@ function circleFirework(x,y) {
     g = random(5,250)
     b = random(5,250)
     for (let i = 0; i < 50; i++) {
-    let p = new Particle(x,y)
-    let angle = random(0,2*PI)
+        let p = new Particle(x,y)
+        let angle = random(0,2*PI)
 
-    let radius = (cos(angle))**2 + (sin(angle))**2
-    let vel = radius
+        let radius = (cos(angle))**2 + (sin(angle))**2
+        let vel = radius
 
-    p.velX = cos(angle) * vel;
-    p.velY = sin(angle) * vel;
+        p.velX = cos(angle) * vel;
+        p.velY = sin(angle) * vel;
 
-    dR = random(-15,15)
-    dG = random(-15,15)
-    dB = random(-15,15)
-    p.r = r + dR
-    p.g = g + dG
-    p.b = b + dB
+        dR = random(-15,15)
+        dG = random(-15,15)
+        dB = random(-15,15)
+        p.r = r + dR
+        p.g = g + dG
+        p.b = b + dB
 
-    particles.push(p)
+        particles.push(p)
     }
 }
